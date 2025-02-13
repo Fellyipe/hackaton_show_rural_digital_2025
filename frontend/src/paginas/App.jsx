6005d64e103daa0ee487a3e5c3908c45e7af6507
TheBud4 <murilopmr13@hotmail.com>
2025-02-13 19:03:12 -0300

    atualizar mensagem de boas-vindas e adicionar botões para agrônomos e produtores

--- frontend/src/paginas/App.jsx
+++ frontend/src/paginas/App.jsx
@@ -7,13 +7,21 @@ function App() {
     return (
       <div className="w-full flex flex-col h-screen items-center justify-center">
         <div className="flex flex-col items-center justify-between h-1/4">
-          <h1 className="text-5xl">Bem Vindo</h1>
-          <button
-            onClick={() => navigate("/analisePotassio")}
-            className="bg-primary w-40 h-10 rounded-md text-white"
-          >
-            Entrar
-          </button>
+          <h1 className="text-3xl">Bem Vindo ao nosso app</h1>
+          <div className='flex flex-col items-center gap-5'>
+            <button
+              onClick={() => navigate("/analisePotassio")}
+              className="bg-primary w-52 h- rounded-md text-white"
+            >
+              Sou Agronomo
+            </button>
+            <button
+              onClick={() => navigate("/analisePotassio")}
+              className="bg-secondary w-40 h-10 rounded-md text-white"
+            >
+              Sou Produtor
+            </button>
+          </div>
         </div>
       </div>
     );
