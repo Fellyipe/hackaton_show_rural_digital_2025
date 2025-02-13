630c819e2c2f8cf774a350fbe601ddd8915cb751
TheBud4 <murilopmr13@hotmail.com>
2025-02-13 20:04:46 -0300

    atualizar tela inicial com logotipo e mensagem de boas-vindas, alterando texto do botão para "Fazer Login"

--- frontend/src/paginas/App.jsx
+++ frontend/src/paginas/App.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+import logo from '../../public/logo.svg';
 
 function App() {
  let navigate = useNavigate();
 
     return (
-      <div className="w-full flex flex-col h-screen items-center justify-center">
-        <div className="flex flex-col items-center justify-between h-1/4">
-          <h1 className="text-3xl">Bem Vindo ao nosso app</h1>
+      <div className="w-full flex flex-col h-screen items-center justify-center gap-6 bg-white-green">
+          <h1 className="text-3xl font-bold text-bold-text">Bem Vindo ao nosso App</h1>
+          <div>
+            <img src={logo} alt="logotipo" />
+          </div>
+          
           <div className="flex flex-col items-center gap-5">
             <button
               onClick={() => navigate("/analisePotassio")}
               className="bg-primary w-52 h-12 rounded-md font-bold text-white text-xl"
             >
-              Sou Agronomo
-            </button>
-            <button
-              onClick={() => navigate("/analisePotassio")}
-              className="bg-secondary w-52 h-12 rounded-md font-bold text-white text-xl"
-            >
-              Sou Produtor
+              Fazer Login
             </button>
           </div>
         </div>
-      </div>
     );
 }
 
