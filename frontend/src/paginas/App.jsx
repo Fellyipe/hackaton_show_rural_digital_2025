96aa0cb9f0d12564e6837dc0ceffef9461ba4248
TheBud4 <murilopmr13@hotmail.com>
2025-02-13 19:06:12 -0300

    ajustar estilo dos botões na página inicial para melhor usabilidade

--- frontend/src/paginas/App.jsx
+++ frontend/src/paginas/App.jsx
@@ -8,16 +8,16 @@ function App() {
       <div className="w-full flex flex-col h-screen items-center justify-center">
         <div className="flex flex-col items-center justify-between h-1/4">
           <h1 className="text-3xl">Bem Vindo ao nosso app</h1>
-          <div className='flex flex-col items-center gap-5'>
+          <div className="flex flex-col items-center gap-5">
             <button
               onClick={() => navigate("/analisePotassio")}
-              className="bg-primary w-52 h- rounded-md text-white"
+              className="bg-primary w-52 h-12 rounded-md font-bold text-white text-xl"
             >
               Sou Agronomo
             </button>
             <button
               onClick={() => navigate("/analisePotassio")}
-              className="bg-secondary w-40 h-10 rounded-md text-white"
+              className="bg-secondary w-52 h-12 rounded-md font-bold text-white text-xl"
             >
               Sou Produtor
             </button>
