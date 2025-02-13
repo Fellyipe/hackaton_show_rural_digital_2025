dd1090f5d3d2523026805aa2f3b47892b20c539b
Fellyipe <felipebueno2004@hotmail.com>
2025-02-13 14:59:35 -0300

    aaaa

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+# Abrir o arquivo PDF
+with open('C:/Users/Felipe/Downloads/RELATORIO DE FREQUENCIA DE DEFESAS.pdf', 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    # Extrair texto de cada página
+    for page in reader.pages:
+        print(page.extract_text())
