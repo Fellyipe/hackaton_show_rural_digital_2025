5581580b8858c27f5fea7b5e517f5b28c5bf8b8e
TheBud4 <murilopmr13@hotmail.com>
2025-02-13 21:32:21 -0300

    ajustar estilo e layout do componente de login; adicionar nova cor 'greenpeace' ao CSS

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -3,14 +3,14 @@ import Footer from '../../components/footer/Footer.jsx';
 
 function Login(){
   return (
-    <div className='flex flex-col items-center h-screen gap-5 p-8 bg-white-green'>
-      <div>
+    <div className="flex flex-col items-center h-screen gap-5 p-8 bg-white-green">
+      <div className='mt-10'>
         <img src={Logo} alt="logotipo" />
       </div>
-      <form className='flex flex-col gap-5 w-full'>
+      <form className="flex flex-col gap-5 w-full">
         <div>
           <input
-            className="appearance-none border-none rounded w-full py-2 px-3 text-gray-700 leading-tight bg-white focus:outline-none"
+            className="appearance-none border-none rounded w-full py-2 px-3 text-gray-700 bg-white focus:outline-none"
             id="email"
             type="email"
             placeholder="Email"
@@ -18,7 +18,7 @@ function Login(){
         </div>
         <div>
           <input
-            className="appearance-none border-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight bg-white focus:outline-none"
+            className="appearance-none border-none rounded w-full py-2 px-3 text-gray-700 mb-3 bg-white focus:outline-none"
             id="password"
             type="password"
             placeholder="Senha"
@@ -26,7 +26,7 @@ function Login(){
         </div>
         <div>
           <button
-            className="bg-primary hover:bg-emerald cursor-pointer text-white font-bold py-2 w-1/2 rounded"
+            className="bg-greenpeace cursor-pointer text-white font-bold py-2 w-full rounded hover:bg-emerald"
             type="submit"
           >
             Entrar
