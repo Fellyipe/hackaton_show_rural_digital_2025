5a12437f55fe1e258b37d6773cc655cda80d4628
TheBud4 <murilopmr13@hotmail.com>
2025-02-13 21:05:43 -0300

    adicionar página de login e atualizar rotas no aplicativo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route,Routes } from "react-router";
-import App from './paginas/App'
-import AnalisePotassio from './paginas/telaAgronomo/AnalisePotassio';
+import App from './Pages/App.jsx';
+import TelaProdutor from './Pages/TelaProdutor/TelaProdutor.jsx';
+import Login from './Pages/Login/Login';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/analisePotassio" element={<AnalisePotassio />} />
+        <Route path="/telaProdutor" element={<TelaProdutor />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
